d8e1226659acc06fc11191a6f23316356a793da2
Luiz Moraes <luizrmm97@gmail.com>
2020-05-29 01:25:29 -0300

    home screen search

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,27 +1,43 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:github_app/stores/home_store.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final _searchController = TextEditingController();
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    final homeStore = Provider.of<HomeStore>(context);
-    TextEditingController _searchControler = TextEditingController();
+    final homeStore = Provider.of<HomeStore>(context, listen: false);
+    print('oi');
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
         centerTitle: true,
       ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Image.asset('assets/github.png'),
-            Padding(
-              padding: EdgeInsets.only(top: 60.0),
-              child: Row(
+      body: SingleChildScrollView(
+        child: Container(
+          constraints: BoxConstraints(
+            minWidth: MediaQuery.of(context).size.width * 0.98,
+          ),
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Image.asset('assets/github.png'),
+              Row(
                 children: <Widget>[
                   Expanded(
                     child: SizedBox(
@@ -33,7 +49,7 @@ class HomeScreen extends StatelessWidget {
                         ),
                         elevation: 4,
                         child: TextField(
-                          controller: _searchControler,
+                          controller: _searchController,
                           decoration: InputDecoration(
                             border: InputBorder.none,
                             hintText: "Pesquisar nome de usuário",
@@ -58,29 +74,114 @@ class HomeScreen extends StatelessWidget {
                     child: Icon(Icons.search),
                     onPressed: () {
                       homeStore.getUserData(homeStore.username);
-                      _searchControler.clear();
+                      _searchController.clear();
                     },
                   ),
                 ],
               ),
-            ),
-            Observer(
-              builder: (context) {
-                switch (homeStore.state) {
-                  case UserFutureState.initial:
-                    return Center(
-                      child: Text('1'),
-                    );
-                  case UserFutureState.loading:
-                    return Text('carregando');
-                  case UserFutureState.loaded:
-                    return Text(homeStore.usermodel.name);
-                  default:
-                    return Text('Container');
-                }
-              },
-            )
-          ],
+              Observer(
+                builder: (context) {
+                  switch (homeStore.state) {
+                    case UserFutureState.initial:
+                      return Padding(
+                        padding: EdgeInsets.only(top: 40),
+                        child: Text(
+                          'Nenhum resultado para exbir!',
+                          textAlign: TextAlign.center,
+                          style: GoogleFonts.lato(
+                              color: Colors.grey[700], fontSize: 20),
+                        ),
+                      );
+                    case UserFutureState.loading:
+                      return Container(
+                        padding: EdgeInsets.only(top: 180),
+                        child: Center(
+                          child: CircularProgressIndicator(),
+                        ),
+                      );
+                    case UserFutureState.loaded:
+                      return Column(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            margin: EdgeInsets.only(top: 40, bottom: 20),
+                            height: 100,
+                            width: 100,
+                            child: CircleAvatar(
+                              backgroundImage:
+                                  NetworkImage(homeStore.usermodel.avatarUrl),
+                            ),
+                          ),
+                          Row(
+                            children: <Widget>[
+                              Expanded(
+                                flex: 1,
+                                child: Text(
+                                  homeStore.usermodel.login,
+                                  style: GoogleFonts.notoSans(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey[800]),
+                                ),
+                              ),
+                              Expanded(
+                                flex: 3,
+                                child: Text(
+                                  homeStore.usermodel.name,
+                                  maxLines: 4,
+                                  overflow: TextOverflow.clip,
+                                  style: GoogleFonts.notoSans(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.grey[700]),
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(
+                            height: 22.0,
+                          ),
+                          Text(
+                            homeStore.usermodel.bio,
+                            maxLines: 4,
+                            overflow: TextOverflow.clip,
+                            style: GoogleFonts.notoSans(
+                                fontSize: 16,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.grey[700]),
+                          ),
+                          SizedBox(
+                            height: 12.0,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              Icon(
+                                Icons.location_on,
+                                size: 20,
+                                color: Colors.red[300],
+                              ),
+                              Text(
+                                homeStore.usermodel.location,
+                                maxLines: 4,
+                                overflow: TextOverflow.clip,
+                                style: GoogleFonts.notoSans(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.grey[700],
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      );
+                    default:
+                      return Text('Container');
+                  }
+                },
+              )
+            ],
+          ),
         ),
       ),
     );
